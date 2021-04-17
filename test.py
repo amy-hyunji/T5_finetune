9dad03785da2fe2b2a8d5165e157c08fb5b8deee
amy-hyunji <alee6868@kaist.ac.kr>
2021-04-17 16:42:40 +0000

    fix test

--- test.py
+++ test.py
@@ -1,24 +1,30 @@
 import torch
 import argparse
-import os 
+import os
 import textwrap
-import logging 
-import pytorch_lightning as pl 
+import logging
+import pytorch_lightning as pl
 import pandas as pd
 
 from transformers import AutoTokenizer, AutoModelWithLMHead
-from dataloader import Trivia_QA_Closedbook, Hotpot_QA_Closedbook, Complex_QA_Closedbook 
-from model import T5FineTuner 
+from dataloader import (
+    Trivia_QA_Closedbook,
+    Hotpot_QA_Closedbook,
+    Complex_QA_Closedbook,
+    LAMA_QA_Closedbook,
+    Qangaroo_QA_Closedbook,
+)
+from model import T5FineTuner
 from utils import set_seed, LoggingCallback, exact_match_score
 from torch.utils.data import Dataset, DataLoader
 
 args_dict = dict(
-    model_name_or_path = "101_t5-base_hotpot_qa_closedbook/best_tfmr",
-    tokenizer_name_or_path = "101_t5-base_hotpot_qa_closedbook/best_tfmr",
-    output_name = "101_t5-base_hotpot_qa",
-    output_dir = "",
-    add_all = True,
-    dataset = "hotpot",
+    model_name_or_path="101_t5-base_hotpot_qa_closedbook/best_tfmr",
+    tokenizer_name_or_path="101_t5-base_hotpot_qa_closedbook/best_tfmr",
+    output_name="101_t5-base_hotpot_qa",
+    output_dir="",
+    add_all=True,
+    dataset="hotpot",
     max_input_length=60,
     max_output_length=20,
     freeze_encoder=False,
@@ -32,32 +38,65 @@ args_dict = dict(
     num_train_epochs=2,
     gradient_accumulation_steps=10,
     n_gpu=1,
-    resume_from_checkpoint=None, 
-    val_check_interval = 0.5, 
+    resume_from_checkpoint=None,
+    val_check_interval=0.5,
     n_val=5000,
     n_train=-1,
     n_test=-1,
     early_stop_callback=False,
-    fp_16=False, # if you want to enable 16-bit training then install apex and set this to true
-    opt_level='O1', # you can find out more on optimisation levels here https://nvidia.github.io/apex/amp.html#opt-levels-and-properties
-    max_grad_norm=1.0, # if you enable 16-bit training then set this to a sensible value, 0.5 is a good default
+    fp_16=False,  # if you want to enable 16-bit training then install apex and set this to true
+    opt_level="O1",  # you can find out more on optimisation levels here https://nvidia.github.io/apex/amp.html#opt-levels-and-properties
+    max_grad_norm=1.0,  # if you enable 16-bit training then set this to a sensible value, 0.5 is a good default
     seed=101,
 )
 
-if args_dict['dataset'] == "trivia":
-    args_dict.update({'num_train_epochs':150,
-                     'train_batch_size': 48, 'eval_batch_size': 48, 'learning_rate': 1e-3,
-                     'resume_from_checkpoint': 't5_trivia_qa_closedbook/checkpointepoch=53.ckpt'})
+if args_dict["dataset"] == "trivia":
+    args_dict.update(
+        {
+            "num_train_epochs": 150,
+            "train_batch_size": 48,
+            "eval_batch_size": 48,
+            "learning_rate": 1e-3,
+            "resume_from_checkpoint": "t5_trivia_qa_closedbook/checkpointepoch=53.ckpt",
+        }
+    )
 elif args_dict["dataset"] == "hotpot":
-    args_dict.update({'num_train_epochs': 100, 
-                    'train_batch_size': 48, 'eval_batch_size': 48, 'learning_rate': 1e-3, 
-                    "resume_from_checkpoint": 'checkpointcheckpoint_ckpt_epoch_19.ckpt'})
-elif args_dict['dataset'] == "complex":
-    args_dict.update({'num_train_epochs':150,
-                     'train_batch_size': 48, 'eval_batch_size': 48, 'learning_rate': 1e-3})
-elif args_dict['dataset'] == "qangaroo":
-    args_dict.update({'num_train_epochs':150,
-                     'train_batch_size': 48, 'eval_batch_size': 48, 'learning_rate': 1e-3})
+    args_dict.update(
+        {
+            "num_train_epochs": 100,
+            "train_batch_size": 48,
+            "eval_batch_size": 48,
+            "learning_rate": 1e-3,
+            "resume_from_checkpoint": "checkpointcheckpoint_ckpt_epoch_19.ckpt",
+        }
+    )
+elif args_dict["dataset"] == "complex":
+    args_dict.update(
+        {
+            "num_train_epochs": 150,
+            "train_batch_size": 48,
+            "eval_batch_size": 48,
+            "learning_rate": 1e-3,
+        }
+    )
+elif args_dict["dataset"] == "lama":
+    args_dict.update(
+        {
+            "num_train_epochs": 150,
+            "train_batch_size": 48,
+            "eval_batch_size": 48,
+            "learning_rate": 1e-3,
+        }
+    )
+elif args_dict["dataset"] == "qangaroo":
+    args_dict.update(
+        {
+            "num_train_epochs": 150,
+            "train_batch_size": 48,
+            "eval_batch_size": 48,
+            "learning_rate": 1e-3,
+        }
+    )
 
 args = argparse.Namespace(**args_dict)
 print(args_dict)
@@ -65,59 +104,119 @@ print(args_dict)
 tokenizer = AutoTokenizer.from_pretrained(args.tokenizer_name_or_path)
 model = T5FineTuner(args)
 model.eval()
-#model = AutoModelWithLMHead.from_pretrained(args.model_name_or_path)
+# model = AutoModelWithLMHead.from_pretrained(args.model_name_or_path)
 
-for split in ['test']:
+for split in ["test"]:
     print(f"Working in {split}")
-    
-    if args_dict['dataset'] == "trivia":
-        dataset = Trivia_QA_Closedbook(tokenizer, split, None, args.max_input_length, args.max_output_length, False, args.add_all)
-    elif args_dict['dataset'] == "hotpot":
-        dataset = Hotpot_QA_Closedbook(tokenizer, split, None, args.max_input_length, args.max_output_length, False, args.add_all)
-    elif args_dict['dataset'] == "complex":
-        dataset = Complex_QA_Closedbook(tokenizer, split, None, args.max_input_length, args.max_output_length, False, args.add_all)
-    elif args_dict['dataset'] == "qangaroo":
-        dataset = Qangaroo_QA_Closedbook(tokenizer, split, None, args.max_input_length, args.max_output_length, False, args.add_all)
+
+    if args_dict["dataset"] == "trivia":
+        dataset = Trivia_QA_Closedbook(
+            tokenizer,
+            split,
+            None,
+            args.max_input_length,
+            args.max_output_length,
+            False,
+            args.add_all,
+        )
+    elif args_dict["dataset"] == "hotpot":
+        dataset = Hotpot_QA_Closedbook(
+            tokenizer,
+            split,
+            None,
+            args.max_input_length,
+            args.max_output_length,
+            False,
+            args.add_all,
+        )
+    elif args_dict["dataset"] == "complex":
+        dataset = Complex_QA_Closedbook(
+            tokenizer,
+            split,
+            None,
+            args.max_input_length,
+            args.max_output_length,
+            False,
+            args.add_all,
+        )
+    elif args_dict["dataset"] == "lama":
+        dataset = LAMA_QA_Closedbook(
+            tokenizer,
+            split,
+            None,
+            args.max_input_length,
+            args.max_output_length,
+            False,
+            args.add_all,
+        )
+    elif args_dict["dataset"] == "qangaroo":
+        dataset = Qangaroo_QA_Closedbook(
+            tokenizer,
+            split,
+            None,
+            args.max_input_length,
+            args.max_output_length,
+            False,
+            args.add_all,
+        )
 
     loader = DataLoader(dataset, batch_size=32, shuffle=False)
     it = iter(loader)
-    #batch = next(it)
-    retdict = {'question': [], 'answer': [], 'predict': [], 'EM': []}
-    
+    # batch = next(it)
+    retdict = {"question": [], "answer": [], "predict": [], "EM": []}
+
     for i, batch in enumerate(it):
         batch["source_ids"].shape
 
-        model.to('cuda')
+        model.to("cuda")
         outs = model.model.generate(
-                    batch["source_ids"].cuda(),
-                    attention_mask=batch["source_mask"].cuda(),
-                    use_cache=True,
-                    decoder_attention_mask=batch['target_mask'].cuda(),
-                    max_length=args.max_output_length,
-                    num_beams=2,
-                    early_stopping=True
-                )
+            batch["source_ids"].cuda(),
+            attention_mask=batch["source_mask"].cuda(),
+            use_cache=True,
+            decoder_attention_mask=batch["target_mask"].cuda(),
+            max_length=args.max_output_length,
+            num_beams=2,
+            early_stopping=True,
+        )
 
-        dec = [tokenizer.decode(ids, skip_special_tokens=True, clean_up_tokenization_spaces=True) for ids in outs]
+        dec = [
+            tokenizer.decode(
+                ids, skip_special_tokens=True, clean_up_tokenization_spaces=True
+            )
+            for ids in outs
+        ]
 
-        texts = [tokenizer.decode(ids, skip_special_tokens=True, clean_up_tokenization_spaces=True) for ids in batch['source_ids']]
-        targets = [tokenizer.decode(ids, skip_special_tokens=True, clean_up_tokenization_spaces=True) for ids in batch['target_ids']]
+        texts = [
+            tokenizer.decode(
+                ids, skip_special_tokens=True, clean_up_tokenization_spaces=True
+            )
+            for ids in batch["source_ids"]
+        ]
+        targets = [
+            tokenizer.decode(
+                ids, skip_special_tokens=True, clean_up_tokenization_spaces=True
+            )
+            for ids in batch["target_ids"]
+        ]
 
-
-        print(f"# of text: {len(retdict['question'])}\nSaving as: {args.output_name}_{split}.txt")
+        print(
+            f"# of text: {len(retdict['question'])}\nSaving as: {args.output_name}_{split}.txt"
+        )
         for i in range(len(texts)):
-            lines = textwrap.wrap(f"{args.dataset} Question:\n%s\n" % texts[i], width=100)
+            lines = textwrap.wrap(
+                f"{args.dataset} Question:\n%s\n" % texts[i], width=100
+            )
             """
             print("\n".join(lines))
             print("\nActual Answer: %s" % targets[i])
             print("\nPredicted Answer from T5: %s" % dec[i])
             print("=====================================================================\n")
             """
-            retdict['question'].append(texts[i])
-            retdict['answer'].append(targets[i])
-            retdict['predict'].append(dec[i])
-            retdict['EM'].append(exact_match_score(dec[i], targets[i]))
+            retdict["question"].append(texts[i])
+            retdict["answer"].append(targets[i])
+            retdict["predict"].append(dec[i])
+            retdict["EM"].append(exact_match_score(dec[i], targets[i]))
 
     df = pd.DataFrame(retdict)
-    #df = df.sort_values(by=['question'], axis=0)
+    # df = df.sort_values(by=['question'], axis=0)
     df.to_csv(f"{args.output_name}_{split}.csv")
